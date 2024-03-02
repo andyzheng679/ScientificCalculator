56e14af62ec3506fe3b76a44d35e5bb7b4d8221b
jake-brennan714 <jake.brennan714@gmail.com>
2024-03-02 13:16:11 -0500

    Added tests (passed) for inverse common log

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestLogarithmSuite.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestLogarithmSuite.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({TestCommonLog.class, TestNaturalLog.class})
+@SelectClasses({TestCommonLog.class, TestNaturalLog.class, TestInverseCommonLog.class})
 public class TestLogarithmSuite {
 
 }
