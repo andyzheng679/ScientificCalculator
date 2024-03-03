e9e870ea5609362f99ca5c1ba036087f145d2628
jake-brennan714 <jake.brennan714@gmail.com>
2024-03-03 16:34:08 -0500

    Started writing tests for cosine

---
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestCosine.java
@@ -0,0 +1,10 @@
+package com.zipcodewilmington.scientific_calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import com.zipcodewilmington.scientificcalculator.Trig;
+
+@DisplayName("Testing Trig.cosine()")
+public class TestCosine {
+}
