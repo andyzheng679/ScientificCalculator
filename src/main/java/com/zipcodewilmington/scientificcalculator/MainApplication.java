015084a58b044a7fb3c31674c463d45cb8682efa
andyzheng679 <andyzheng679@gmail.com>
2024-03-03 21:44:08 -0500

    while true loop

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -18,7 +18,38 @@ public class MainApplication {
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a double", d);
 
+        Scanner scanner = new Scanner(System.in);
+        ScientificCalculator calculator = new ScientificCalculator(0);
+
+        while(true){
+            System.out.println("Here is the current display value: " + calculator.getDisplayValue());
+            System.out.println("Enter new display value: ");
+            double input = scanner.nextDouble();
+            calculator.changeDisplay(input);
+            System.out.println("New display value: " + calculator.getDisplayValue());
+            System.out.println("Pick operation: +, -, *, /, squared, sqrt, exponentiation, inverse, invertSign, clear, or exit: ");
+
+            String operationInput = scanner.next();
+
+            if(operationInput.equalsIgnoreCase("exit")){
+                break;
+            }
+
+            switch(operationInput.toLowerCase()){
+                //..
+
+
+            }
+
+            //
+
+        }
+
+
+
+
     }
+
 
 }
 
